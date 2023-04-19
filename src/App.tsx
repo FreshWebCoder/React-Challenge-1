b33e49862ef89b55dd0b98cea84e54168a5f856a
FreshWebCoder <freshwebcoder@hotmail.com>
2023-04-20 03:46:41 +0800

    complete size adjustment feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import { useState } from 'react';
+import type { MouseEvent } from 'react';
 
 import type { RotationType } from './types';
 
 import logo from './logo.svg';
 import './App.css';
 
+const minIconHeight = Math.min(window.innerHeight, window.innerWidth) * 0.1;
+const maxIconHeight = Math.min(window.innerHeight, window.innerWidth) * 0.5;
+const ratio = (maxIconHeight - minIconHeight) / (window.innerHeight + window.innerWidth);
+
 function App() {
+  const [height, setHeight] = useState<number>(maxIconHeight);
   const [rotation, setRotation] = useState<RotationType>('normal');
 
   const onClickIcon = () => {
     setRotation(rotation === 'normal' ? 'reverse' : 'normal');
   };
 
+  const onMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    setHeight(minIconHeight + (e.clientX + e.clientY) * ratio);
+  };
+
   return (
-    <div className="App">
+    <div className="App" onMouseMove={onMouseMove}>
       <header className="App-header">
         <img
           src={logo}
           className="App-logo"
           alt="logo"
-          style={{ animationDirection: rotation }}
+          style={{ animationDirection: rotation, height }}
           onClick={onClickIcon}
         />
         <p>
